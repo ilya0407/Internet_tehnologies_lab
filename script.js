28fc06ee1672795bff6b2f684be9c09539f2ecda
ilya0407 <42876117+ilya0407@users.noreply.github.com>
2021-06-12 13:50:36 +0300

    lab9

---
+++ script.js
@@ -0,0 +1,30 @@
+const skills = [
+    {name : "Java", level : 60, css_class: "skill-java"},
+    {name : "SQL", level : 60, css_class: "skill-sql"},
+    {name : "HTML", level : 60, css_class: "skill-html"},
+    {name : "CSS", level : 60, css_class: "skill-css"},
+    {name : "C++", level : 60, css_class: "skill-cpp"},
+];
+
+function input_skills(){
+    const dl = document.createElement('dl');
+    dl.classList.add('skills-list');
+
+    skills.forEach(ìtem => {
+        const dt = document.createElement('dt');
+        dt.textContent = item.name;
+        dt.classList.add(item.css_class);
+        const dd = document.createElement('dd');
+        dd.classList.add('level');
+        const div = document.createElement('div');
+        div.style.width = item.level + '%';
+        div.textContent = item.level;
+        dd.appendChild(div);
+        dl.append(dt);
+        dl.append(dd);
+    });
+    document.getElementsById('skills').append(dl);
+}
+
+input_skills();
+
